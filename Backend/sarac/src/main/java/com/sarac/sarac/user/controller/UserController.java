cbdc775387ef456f9a083f9e44fc11d824a92d74
thwjd793@naver.com <thwjd793@naver.com>
2022-09-15 15:00:46 +0900

    :sparkles: feat :  프로필 사진 주소

--- Backend/sarac/src/main/java/com/sarac/sarac/user/controller/UserController.java
+++ Backend/sarac/src/main/java/com/sarac/sarac/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.sarac.sarac.user.entitiy.User;
 import com.sarac.sarac.user.service.UserService;
 import com.sarac.sarac.user.util.JwtUtil;
 import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -37,6 +39,7 @@ public class UserController {
 
     @GetMapping("/me")
     public ResponseEntity<Map<String, Object>> myProfile(@RequestHeader Map<String, Object> header) {
+        log.info("**********************"+(String) header.get("authorization"));
         Map<String, Object> resultMap = new HashMap<>();
         Claims claims = jwtUtil.parseJwtToken((String)header.get("authorization"));
         resultMap.put("id", claims.get("id"));
@@ -45,12 +48,12 @@ public class UserController {
         return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
     }
 
-    @GetMapping("/auth/verify")
-    public ResponseEntity<Void> verifyToken(@RequestHeader Map<String, Object> header) {
-        String token = (String) header.get("authorization");
-//        log.info("토큰 : {}", token);
-        return new ResponseEntity<Void>(jwtUtil.validateJwtToken(token));
-    }
+//    @GetMapping("/auth/verify")
+//    public ResponseEntity<Void> verifyToken(@RequestHeader Map<String, Object> header) {
+//        String token = (String) header.get("authorization");
+////        log.info("토큰 : {}", token);
+//        return new ResponseEntity<Void>(jwtUtil.validateJwtToken(token));
+//    }
 
     @DeleteMapping
     public ResponseEntity<?> deleteUser(@RequestHeader Map<String,Object> token) {
@@ -65,6 +68,16 @@ public class UserController {
         return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
     }
 
-
+    @GetMapping("/ImagePath")
+    public ResponseEntity<Map<String, String>> fileView(@RequestHeader Map<String,Object> token) {
+        Map<String, String> resultMap = new HashMap<>();
+        User image = userService.findByKakaoId((Long)jwtUtil.parseJwtToken((String) token.get("authorization")).get("id"));
+        if (image.getImagePath() ==null) {
+            resultMap.put("message", "등록된 사진이 없습니다.");
+        } else {
+            resultMap.put("image_path",image.getImagePath());
+        }
+        return new ResponseEntity<>(resultMap, HttpStatus.OK);
+    }
 
 }
