6a994fd325a48e1a0c03eb5555508292c36955c8
정다은 <danijde71@naver.com>
2022-09-23 19:12:30 +0900

    :sparkles: feat : myfeed Library feature added
    userInfoRes 이름 재편집, 서재 api 구현중

--- Backend/sarac/src/main/java/com/sarac/sarac/myfeed/service/MyFeedService.java
+++ Backend/sarac/src/main/java/com/sarac/sarac/myfeed/service/MyFeedService.java
@@ -1,12 +1,17 @@
 package com.sarac.sarac.myfeed.service;
 
+import com.sarac.sarac.myfeed.dto.response.MyFeedLibraryRes;
 import com.sarac.sarac.myfeed.dto.response.MyFeedUserRes;
-import com.sarac.sarac.myfeed.dto.response.MyFeedUserResInfoRes;
+import com.sarac.sarac.myfeed.dto.response.MyFeedUserInfoRes;
+import com.sarac.sarac.user.entitiy.User;
 
 import java.util.List;
 
 public interface MyFeedService {
-    MyFeedUserResInfoRes getInfoByUserInfo(Long userId);
+    MyFeedUserInfoRes getInfoByUserInfo(Long userId);
 
     List<MyFeedUserRes> getUserList(String keyWord);
+
+    List<MyFeedLibraryRes> getBookList(Long userId);
+
 }
