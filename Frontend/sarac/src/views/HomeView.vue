770abbc269b8709a26ebb3cd082ce5f86fab589d
rudwns06 <rudwns06@hanmail.net>
2022-09-23 13:52:51 +0900

    ✨ feat   : 피드 모아보기 FE (컴포넌트 구현 및 데이터 연결) #S07P22A505-175

--- Frontend/sarac/src/views/HomeView.vue
+++ Frontend/sarac/src/views/HomeView.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
     <p>개인리뷰모아보기 home이요~</p>
+    <feeds :reviewList="randomReviewList"></feeds>
   </div>
 </template>
 
 <script>
+// import { mapActions } from "vuex";
+// const accountStore = "accountStore";
+
+import axios from "axios";
+
+import feeds from "../components/homeview/feeds.vue";
 export default {
   name: "Home",
 
-  components: {},
+  components: { feeds },
+  data() {
+    return {
+      randomReviewList: null,
+    };
+  },
+
+  created() {
+    const accessToken = localStorage.getItem("accessToken");
+    console.log("homeview created");
+    axios({
+      url: "api/v1/review/feeds/1",
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }).then((res) => {
+      console.log(res);
+      this.randomReviewList = res.data;
+    });
+  },
 };
 </script>
