3c3d81152e86de67da1fa87e06f3dbe46f5fd6cf
정다은 <danijde71@naver.com>
2022-09-23 13:49:19 +0900

    :sparkles: feat : edit ReviewListDTO
    리뷰id, 리뷰사진리스트 추가

--- Backend/sarac/src/main/java/com/sarac/sarac/review/payload/response/ReviewListDTO.java
+++ Backend/sarac/src/main/java/com/sarac/sarac/review/payload/response/ReviewListDTO.java
@@ -1,16 +1,22 @@
 package com.sarac.sarac.review.payload.response;
 
+import com.sarac.sarac.review.entity.ReviewPhoto;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
+@Builder
 public class ReviewListDTO {
+    private Long reviewId;
 
     private String bookTitle;
 
     private String title;
 
-
+    private List<ReviewPhoto> photoUrlList;
 
 }
