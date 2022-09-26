117568457657b4832ce1be78871b37d960184e8c
rudwns06 <rudwns06@hanmail.net>
2022-09-26 10:25:52 +0900

    ✨ feat   : 리뷰상세보기 (데이터 연결 완료) #S07P22A505-172

---
+++ Frontend/sarac/src/views/DetailReview.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    {{ review }}
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+const reviewStore = "reviewStore";
+
+export default {
+  name: "detailReview",
+  props: {
+    reviewId: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      review: null,
+    };
+  },
+  methods: {
+    ...mapActions(reviewStore, ["getDetailReview"]),
+    async getReview(reviewId) {
+      this.review = await this.getDetailReview(reviewId);
+    },
+  },
+  created() {
+    console.log(this);
+    console.log(this.reviewId);
+    this.getReview(this.reviewId);
+  },
+};
+</script>
+
+<style></style>
