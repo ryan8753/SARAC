5aa9c34c0a542db5b35078549e42570c4148e96e
신민아 <minah741@naver.com>
2022-09-27 14:33:31 +0900

    :sparkles: feat: IMPL searching (best) books in FE
    - 리뷰 많은 순으로 인기도서 가져오기 및 책 검색 fe 페이지 구현 완료.
    - 추가적인 디자인 필요.
    - prop를 통해 해당 책이 인기 도서인지 검색 결과인지 노출 필요.

---
+++ Frontend/sarac/src/components/search/BookSearchList.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <book-search-item
+      v-for="book in searchResults.results"
+      :key="book.isbn"
+      v-bind="book"
+    ></book-search-item>
+  </v-container>
+</template>
+
+<script>
+import { mapGetters, mapState } from "vuex";
+const searchStore = "searchStore";
+
+import BookSearchItem from "./BookSearchItem.vue";
+
+export default {
+  components: { BookSearchItem },
+  computed: {
+    ...mapState(["searchResults"]),
+    ...mapGetters(searchStore, ["searchResults"]),
+  },
+};
+</script>
---
+++ Frontend/sarac/src/views/SearchView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <search-bar></search-bar>
+    <book-search-list></book-search-list>
+  </div>
+</template>
+<script>
+import SearchBar from "@/components/search/SearchBar.vue";
+import BookSearchList from "@/components/search/BookSearchList.vue";
+
+import { mapActions, mapState } from "vuex";
+const searchStore = "searchStore";
+
+export default {
+  components: { SearchBar, BookSearchList },
+  computed: {
+    ...mapState(["searchResults"]),
+  },
+  mounted() {
+    this.getBestBook();
+  },
+  methods: {
+    ...mapActions(searchStore, ["getBestBook"]),
+  },
+};
+</script>
