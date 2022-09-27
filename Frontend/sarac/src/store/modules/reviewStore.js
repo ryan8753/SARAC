58b178e61165adf8cbc1fa283d6e78f9a9c3be53
thwjd793@naver.com <thwjd793@naver.com>
2022-09-27 17:26:02 +0900

    :sparkles: feat : 해시태그 저장

--- Frontend/sarac/src/store/modules/reviewStore.js
+++ Frontend/sarac/src/store/modules/reviewStore.js
@@ -59,12 +59,11 @@ const reviewStore = {
       console.log(files);
       const formData = new FormData();
       formData.append('review', new Blob([ JSON.stringify(review) ], {type : "application/json"}));
-      // formData.append('review', review);
       
       for(let i=0; i<files.length ;i++){
         formData.append("files", files[i]);
       }
-      // formData.append("files", files);
+    
       const accessToken = localStorage.getItem("accessToken");
       const response = await axios({
         url: "api/v1/review",
@@ -84,22 +83,25 @@ const reviewStore = {
       });
       return response;
     },
-    async updateReview() {
+    async updateReview(context,{review,files}) {
+      context;
       const formData = new FormData();
-      // formData.append('review', new Blob([ JSON.stringify(data) ], {type : "application/json"}));
-      // formData.append("files", photoFile.files[0]);
+      formData.append('review', new Blob([ JSON.stringify(review) ], {type : "application/json"}));
+      
+      for(let i=0; i<files.length ;i++){
+        formData.append("files", files[i]);
+      }
       const accessToken = localStorage.getItem("accessToken");
       const response = await axios({
         url: "api/v1/review/update",
         method: "PUT",
         headers: {
           Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'multipart/form-data',
+          // 'Content-Type': 'multipart/form-data',
         },
         data:{
           formData,
         },
-
         
       }).then((res) => {
         console.log(res);
