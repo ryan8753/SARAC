f78834f05992325b40b7c83b10132156a597ac8b
정다은 <danijde71@naver.com>
2022-09-28 10:14:12 +0900

    :sparkles: feat : add myfeed userinfo FE #S07P22A505-166
    myfeed 사용자 상세 페이지 구현중 및 필요한 페이지 라우팅중

--- Backend/sarac/src/main/java/com/sarac/sarac/user/dto/UserDto.java
+++ Backend/sarac/src/main/java/com/sarac/sarac/user/dto/UserDto.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDto {
+    private Long userId;
+
     private Long kakaoId;
 
     private String nickname;
--- Frontend/sarac/src/components/MyFeedView/UserInfo.vue
+++ Frontend/sarac/src/components/MyFeedView/UserInfo.vue
@@ -1,50 +1,62 @@
 <template>
-  <div>
+  <v-container>
     <v-row>
-    <v-col>
+    <v-col cols="3">
         <v-avatar>
-      <img
-        :src="this.userInfo.imagePath"
+      <v-img
+        :src="userInfo.imagePath"
         alt="profile"
-        size="128"
-      >
+      ></v-img>
     </v-avatar>
     </v-col>
-    <v-col>
-        상세정보
+    <v-col cols="9">
+      <v-sheet
+      color="#F3EDED"
+      class="rounded-lg"
+      height="10vh"
+      >
+      <v-row>
+        <v-col><b>읽은책</b><br />{{userInfo.read}}</v-col>
+        <v-col><b>읽는중</b><br />{{userInfo.reading}}</v-col>
+        <v-col><b>읽을책</b><br />{{userInfo.wish}}</v-col>
+      </v-row>
+      </v-sheet>
     </v-col>
     </v-row>
-  </div>
+  </v-container>
 </template>
 
 <script>
-// import { mapState } from 'vuex';
-// import myFeedStore from '../../store/modules/myFeedStore';
+import { mapState } from 'vuex';
+
+const myFeedStore = "myFeedStore";
 
 export default {
     name: "UserInfo",
     data() {
       return {
-        // userId: 1,
-
+        items:[],
       }
     },
-    props: ["userInfo"],
     computed: {
-        // ...mapState(myFeedStore, ["userInfo"]),
+        ...mapState(myFeedStore, ["userInfo"]),
     },
     methods: {
         // ...mapActions(myFeedStore, ["getUserInfo"]),
 
     },
     created() {
-      // this.$router.go(this.$router.currentRoute);
-        // this.$store.dispatch(myFeedStore, "getUserInfo", 1); // 우선 내 정보
-        // console.log("sss")
-    }
+      this.items.push({title: "읽은 책", subTitle: this.userInfo.read});
+      this.items.push({title: "읽는 중인 책", subTitle: this.userInfo.reading});
+      this.items.push({title: "읽을 책", subTitle: this.userInfo.wish});
+      
+},
 }
 </script>
 
-<style>
-
+<style scoped>
+* {
+  font-size: 1vh;
+  text-align: center;
+}
 </style>
