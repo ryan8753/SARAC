0c3d1e8f32a6ba175306eaab5e500bb536ad2ac1
김창현 <ckdgus8753@gmail.com>
2022-09-29 15:03:19 +0900

    :sparkles: feat: 책상세구현중

--- Frontend/sarac/src/views/BookDetailView.vue
+++ Frontend/sarac/src/views/BookDetailView.vue
@@ -1,82 +1,59 @@
 <template>
-  <div v-if="book" class="book-detail-container"> 
-    <book-search-item
-      v-bind="book"
-    ></book-search-item>
-    <v-btn-toggle
-      dense
-      borderless
-      v-model="book.libraryType"
-    >
-      <v-btn value="wish">
-        읽고싶은책
-      </v-btn>
+  <div v-if="book" class="book-detail-container">
+    <book-search-item v-bind="book"></book-search-item>
+    <v-btn-toggle dense borderless v-model="book.libraryType">
+      <v-btn value="wish"> 읽고싶은책 </v-btn>
 
-      <v-btn value="reading">
-        읽는중
-      </v-btn>
-      <v-btn value="read">
-        다 읽었어요
-      </v-btn>
+      <v-btn value="reading"> 읽는중 </v-btn>
+      <v-btn value="read"> 다 읽었어요 </v-btn>
     </v-btn-toggle>
 
-    <v-btn-toggle
-      dense
-      borderless
-      v-model="detailOrReviews"
-    >
-      <v-btn value="detail">
-        상세정보
-      </v-btn>
+    <v-btn-toggle dense borderless v-model="detailOrReviews">
+      <v-btn value="detail"> 상세정보 </v-btn>
 
-      <v-btn value="review">
-        리뷰
-      </v-btn>
+      <v-btn value="review"> 리뷰 </v-btn>
     </v-btn-toggle>
-    <hr>
-    <div v-show="detailOrReviews==`detail`">
-      책 정보
-    </div>
-    <div v-show="detailOrReviews==`review`">
-      책 리뷰
-    </div>
+    <hr />
+    <BookDescription v-show="detailOrReviews == `detail`" v-bind:book="book"
+      >책 정보</BookDescription
+    >
+    <div v-show="detailOrReviews == `review`">책 리뷰</div>
   </div>
-  <div v-else>
-    책정보가없습니다 다시 시도해주세요
-  </div>
+  <div v-else>책정보가없습니다 다시 시도해주세요</div>
 </template>
 
 <script>
-import BookSearchItem from '@/components/search/BookSearchItem.vue';
-import { mapActions, mapState } from 'vuex';
+import BookSearchItem from "@/components/search/BookSearchItem.vue";
+import BookDescription from "@/components/bookDetail/bookDescription.vue";
+import { mapActions, mapState } from "vuex";
 
 const bookStore = "bookStore";
-const accountStore = "accountStore"
+const accountStore = "accountStore";
 export default {
   name: "bookDetail",
-  components : { BookSearchItem },
-  computed:{
-    ...mapState(accountStore,["user"]),
-    ...mapState(bookStore,["book"])
+  components: { BookSearchItem, BookDescription },
+  computed: {
+    ...mapState(accountStore, ["user"]),
+    ...mapState(bookStore, ["book"]),
   },
-  methods : {
-    ...mapActions(bookStore,["getBookDetail","editReadStatus", "deleteReadStatus"])
+  methods: {
+    ...mapActions(bookStore, [
+      "getBookDetail",
+      "editReadStatus",
+      "deleteReadStatus",
+    ]),
   },
-  
-  
-  data () {
+
+  data() {
     return {
       detailOrReviews: "detail",
-    }
+    };
   },
-  
+
   created() {
-
-  }
-
-
-
-  }
+    this.getBookDetail(1234);
+  },
+};
 </script>
 
 <style>
@@ -93,9 +70,9 @@ export default {
 }
 
 hr {
-  background-color: rgba(243,237,237,1);
+  background-color: rgba(243, 237, 237, 1);
   /* background-color: blue; */
   height: 1px;
-  width:100%;
+  width: 100%;
 }
 </style>
