b6edd90338e220725918edd35baed2833d3b30cb
thwjd793@naver.com <thwjd793@naver.com>
2022-09-30 17:18:52 +0900

    :sparckles: feat: 통계프론트 기초

--- Frontend/sarac/src/components/MyFeedView/UserStatistics.vue
+++ Frontend/sarac/src/components/MyFeedView/UserStatistics.vue
@@ -1,11 +1,47 @@
 <template>
   <v-container>
-    통계통계
+    워드클라우드
+    <br/>
+    <br/>
+    <br/>
+    내 서재의 총 가격 : {{totalPrice}}
+    <br/>
+    내가 준 평균 평점 : {{totalScore}}
+    <br/>
+    소음 측정 횟수 :
+    <br/>
+    <br/>
+    <br/>
+    많이 쓴 해쉬태그
   </v-container>
 </template>
 
 <script>
+import { mapActions,  } from "vuex";
+const statisticsStore = "statisticsStore";
+
+
+
 export default {
+ name: "UserStatistics",
+  
+ data() {
+    return {
+      totalPrice: "",
+      totalScore: ""
+
+    };
+  },
+  computed: {
+    
+  },
+  methods: {
+    ...mapActions(statisticsStore, ["getStatistics"
+    ]),
+
+  
+  },
+
 
 }
 </script>
