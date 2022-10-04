712404c3b1a4bdfbeab6b5322ad7ab68c44eaa8d
ryan8753 <ckdgus8753@gmail.com>
2022-10-05 03:56:20 +0900

    :hammer: chore: bookDetail edited
    목차,페이지,장르 추가

--- Frontend/sarac/src/components/bookDetail/bookDescription.vue
+++ Frontend/sarac/src/components/bookDetail/bookDescription.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <div class="description-container">
-      <h2>책소개</h2>
-      <p>저자: {{ this.book.author }}</p>
-      <p>출판사: {{ this.book.publisher }}</p>
-      <p>정가: {{ this.book.price }} 원</p>
+      <h2>책 소개</h2>
+      <p>저자 : {{ this.book.author }}</p>
+      <p>출판사 : {{ this.book.publisher }}</p>
+      <p v-if="this.book.price !=0">정가 : {{ this.book.price }} 원</p>
+      <p v-if="this.book.genre">장르 : {{ this.book.genre}}</p>
+      <p v-if="this.book.page !=0">페이지 수 : {{ this.book.page}} 장</p>
     </div>
     <hr />
+    <div v-if="this.book.toc" class="description-container">
+    <h2 >목차</h2>
+    <div id='toc'></div>
+    </div>
     <div class="description-container">
       <h2>줄거리</h2>
       <p>{{ this.book.description }}</p>
@@ -18,6 +24,10 @@
 <script>
 export default {
   props: ["book"],
+  mounted() {
+    const toc = document.querySelector("#toc")
+    toc.innerHTML=this.book.toc   
+  }
 };
 </script>
 <style scoped>
@@ -34,5 +44,8 @@ p {
 }
 .description-container {
   padding-left: 5%;
+}
+#toc {
+  font-size:1vh; 
 }
 </style>
