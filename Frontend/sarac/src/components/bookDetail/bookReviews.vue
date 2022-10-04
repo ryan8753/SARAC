e89dd8d21c0880d93bd627aca7827601844d44c9
양소정 <thwjd793@naver.com>
2022-10-04 11:56:12 +0900

    :bug: bug: 책상세 수정버튼 위치 수정

--- Frontend/sarac/src/components/bookDetail/bookReviews.vue
+++ Frontend/sarac/src/components/bookDetail/bookReviews.vue
@@ -15,7 +15,7 @@
           v-for="review in reviewList"
           :key="review.reviewId"
           v-model="reviewList"
-          class="px-0"
+          class="px-0 col-card"
         >
           <v-card
             class="mx-0"
@@ -96,5 +96,9 @@ export default {
 h1 {
   font-size: 3vh;
   font-weight: bold;
+}
+.col-card {
+  display: flex;
+  justify-content: center;
 }
 </style>
