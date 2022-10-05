cfbb7d2f18728aa1062697c792b41bde307c5bce
신민아 <minah741@naver.com>
2022-10-05 21:42:28 +0900

    :bug: fix: FIX Korean Regex
    - 한국어 데이터만 제대로 가져오도록 정규식 수정.

--- Data/src/main/java/com/sarac/data/Wordcount.java
+++ Data/src/main/java/com/sarac/data/Wordcount.java
@@ -27,7 +27,7 @@ public class Wordcount {
             StringTokenizer itr;
 
             while(lineItr.hasMoreTokens()) {
-                String line = lineItr.nextToken().replaceAll("[^ㄱ-ㅣ가-힣]", " ");
+                String line = lineItr.nextToken().replaceAll("[^\uAC00-\uD7A3]", " ");
 
                 if(line.trim().length() == 0)
                     continue;
