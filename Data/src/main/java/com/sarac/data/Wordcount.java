1c902a3f41759c28c6789be07a0d569fb4a7fdab
신민아 <minah741@naver.com>
2022-10-05 19:36:20 +0900

    :recycle: refactor: REMOVE remarks
    - 주석 삭제.

--- Data/src/main/java/com/sarac/data/Wordcount.java
+++ Data/src/main/java/com/sarac/data/Wordcount.java
@@ -15,17 +15,11 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Wordcount {
-    /*
-    Object, Text : input key-value pair type (always same (to get a line of input file))
-    Text, IntWritable : output key-value pair type
-    */
     public static class TokenizerMapper
             extends Mapper<Object,Text,Text,IntWritable> {
 
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
-
-        // map function (Context -> fixed parameter)
         public void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
 
@@ -52,30 +46,10 @@ public class Wordcount {
                 }
             }
         }
-
-//        private String clear(String line) {
-//            return line
-//                    .replaceAll("&lt;", " ")
-//                    .replaceAll("&gt;", " ")
-//                    .replaceAll("&quot;", " ")
-//                    .replaceAll(".", " ")
-//                    .replaceAll(",", " ")
-//                    .replaceAll("'", " ");
-//        }
     }
-
-    /*
-    Text, IntWritable : input key type and the value type of input value list
-    Text, IntWritable : output key-value pair type
-    */
     public static class IntSumReducer
             extends Reducer<Text,IntWritable,Text,IntWritable> {
-
-        // variables
         private IntWritable result = new IntWritable();
-
-        // key : a disticnt word
-        // values :  Iterable type (data list)
         public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
 
@@ -88,8 +62,6 @@ public class Wordcount {
         }
     }
 
-
-    /* Main function */
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
@@ -102,17 +74,14 @@ public class Wordcount {
         Job job = new Job(conf,"wordcount");
         job.setJarByClass(Wordcount.class);
 
-        // let hadoop know my map and reduce classes
         job.setMapperClass(TokenizerMapper.class);
         job.setReducerClass(IntSumReducer.class);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-        // set number of reduces
         job.setNumReduceTasks(2);
 
-        // set input and output directories
         FileInputFormat.addInputPath(job,new Path(otherArgs[0]));
         FileOutputFormat.setOutputPath(job,new Path(otherArgs[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1 );
