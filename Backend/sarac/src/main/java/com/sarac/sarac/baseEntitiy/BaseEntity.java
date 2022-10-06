57565830c77e788111ee5afb914f55354c18991c
남궁준 <melon0soda@gmail.com>
2022-10-07 08:16:53 +0900

    :sparkles: feat : 지도 및 좋아요 기능 심화 - 지도 로딩 시 평균 소음을 기준으로 카페 아이콘의 색상 변화 - 카페 아이콘 책으로 변경 - 각 카페의 좋아요/싫어요 카운팅 및 실시간 반영

--- Backend/sarac/src/main/java/com/sarac/sarac/baseEntitiy/BaseEntity.java
+++ Backend/sarac/src/main/java/com/sarac/sarac/baseEntitiy/BaseEntity.java
@@ -1,7 +1,6 @@
 package com.sarac.sarac.baseEntitiy;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
