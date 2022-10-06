1dd34297a72739028058b37d528c3c2421baaf12
SSAFY <danijde71@naver.com>
2022-10-06 20:26:20 +0900

    :sparkles: feat : pagenation location query complete #S07P22A505-283
    페이지네이션 구현 중

--- Frontend/sarac/src/components/search/SearchBar.vue
+++ Frontend/sarac/src/components/search/SearchBar.vue
@@ -6,11 +6,12 @@
           <v-text-field
             hide-details
             v-model="keyword"
+            prepend-inner-icon="mdi-menu-down"
+            @click:prepend-inner="dropdown"
             append-icon="mdi-magnify"
             append-outer-icon="mdi-camera"
             filled
             clearable
-            type="text"
             placeholder="검색어 입력"
             @click:append="searchBook"
             @click:append-outer="getPicture"
@@ -19,6 +20,16 @@
             @keydown.enter.prevent="searchBook"
             id="bar"
           ></v-text-field>
+          <v-menu offset-y v-model="showMenu">
+            <v-list>
+              <v-list-item
+                v-for="(item, index) in items"
+                :key="index"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
         </v-col>
       </v-row>
       <v-row v-if="showCamera">
@@ -37,12 +48,20 @@ export default {
   data: () => ({
     keyword: "",
     showCamera: false,
+    items: [{ title: "Click Me" }, { title: "Click Me 2" }],
+    showMenu: false,
   }),
+  props:["page"],
   components: {
     StreamBarcodeReader,
   },
   computed: {
     ...mapState(["searchResults"]),
+  },
+  watch: {
+    page(newVal) {
+      this.getBookResults({keywork: this.keyword, page: newVal});
+    }
   },
   methods: {
     ...mapActions(searchStore, ["getBookResults"]),
@@ -57,15 +76,15 @@ export default {
         alert("2글자 이상 입력해주세요.");
         return;
       }
+
+      // 타입 추가!!!
+      this.$router.push("/search?keyword=" + this.keyword + "&page=" + this.page).catch(() => {});
       this.setTypeTrue();
-      this.getBookResults(this.keyword);
+      this.getBookResults({keyword: this.keyword, page: "0"});
       document.getElementById("bar").blur();
     },
     getPicture() {
       this.showCamera = !this.showCamera;
-    },
-    clearKeyword() {
-      this.keyword = "";
     },
     onDecode(text) {
       // TODO : isbn으로 백엔드 검색 후 책 결과가 없다면 페이지 이동 x
@@ -73,6 +92,9 @@ export default {
       this.keyword = text;
       this.showCamera = false;
       this.$router.push({ path: "/book/detail/" + this.keyword });
+    },
+    dropdown() {
+      this.showMenu = true;
     },
   },
 };
